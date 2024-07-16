d2f993cbcf1d94034b481d89dead1c115d5c1ef7
djlamey <76072289+djlamey@users.noreply.github.com>
2024-07-16 14:02:02 -0400

    Update SSLO-C01L1-ProvNet.sh

--- SSLO-C01L1-ProvNet.sh
+++ SSLO-C01L1-ProvNet.sh
@@ -3,7 +3,7 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 # update Student Workstation host for the specific lab
-mkdir Desktop/Lab_Files
+touch /tmp/lab1.txt
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
