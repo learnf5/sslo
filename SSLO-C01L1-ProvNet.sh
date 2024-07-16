36f8d26b9b3da5a4456660d47e0206ff7fb9b5ac
djlamey <76072289+djlamey@users.noreply.github.com>
2024-07-16 12:49:55 -0400

    Create SSLO-C01L1-ProvNet.sh

---
+++ SSLO-C01L1-ProvNet.sh
@@ -0,0 +1,12 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update Student Workstation host for the specific lab
+mkdir Desktop/Lab_Files
+
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# update nginx host for the specific lab
+# NOTHING specific for this lab - only the common lab files from common-tasks.sh
