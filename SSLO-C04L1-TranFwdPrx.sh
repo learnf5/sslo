9dbbd51bc8364cdb53c183fdaad221dfd0e7d8ae
djlamey <76072289+djlamey@users.noreply.github.com>
2024-07-16 14:41:20 -0400

    Update SSLO-C04L1-TranFwdPrx.sh
    code corrections

--- SSLO-C04L1-TranFwdPrx.sh
+++ SSLO-C04L1-TranFwdPrx.sh
@@ -3,16 +3,16 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 #Download needed files
-curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/cert/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
-curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/sslo1_cert.ucs     --output /tmp/sslo1_cert.ucs
+ucs=sslo1_cert.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
+curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs     --output /tmp/$ucs
 
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
 #prepare sslo1
-
-sudo scp /tmp/sslo1_cert.ucs 192.168.1.31:/var/local/ucs
-sudo ssh 192.168.1.31 tmsh load sys ucs sslo1_cert.ucs
+sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
+sudo ssh 192.168.1.31 tmsh load sys ucs $ucs
 
 # update Student Workstation
 touch /tmp/lab2.1
@@ -20,5 +20,3 @@ sudo ip route change default via 172.16.1.33
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
-
-# NOTHING specific for this lab - only the common lab files from common-tasks.sh
