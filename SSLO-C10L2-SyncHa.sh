5ff301165706efd9627e3673aee2ce94904f1883
djlamey <76072289+djlamey@users.noreply.github.com>
2024-07-17 11:30:22 -0400

    Update SSLO-C10L2-SyncHa.sh

--- SSLO-C10L2-SyncHa.sh
+++ SSLO-C10L2-SyncHa.sh
@@ -7,7 +7,7 @@ ucs1=sslo1_ha.ucs
 ucs2=sslo2_ha.ucs
 curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
 curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs1     --output /tmp/$ucs1
-curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs2     --output /tmp/$ucs1
+curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs2     --output /tmp/$ucs2
 
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
