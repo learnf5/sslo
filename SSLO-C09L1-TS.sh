cf0680c0405801d6decedeecafc5306c88511a66
djlamey <76072289+djlamey@users.noreply.github.com>
2024-07-17 11:22:00 -0400

    Update SSLO-C09L1-TS.sh

--- SSLO-C09L1-TS.sh
+++ SSLO-C09L1-TS.sh
@@ -12,7 +12,7 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 
 #prepare sslo1
 sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
-sudo ssh 192.168.1.31 tmsh load sys ucs $ucs
+sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
 
 # update Student Workstation
 touch /tmp/lab9.1
