69ef5a8fcdde6262bdf8117029891de53c92046d
djlamey <76072289+djlamey@users.noreply.github.com>
2024-09-18 15:58:35 -0400

    Update SSLO-C01L1-ProvNet.sh

--- SSLO-C01L1-ProvNet.sh
+++ SSLO-C01L1-ProvNet.sh
@@ -9,3 +9,6 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 touch /tmp/lab1.txt
 
 # NOTHING specific for this lab - only the common lab files from common-tasks.sh
+
+# disable debugging
+set +x
