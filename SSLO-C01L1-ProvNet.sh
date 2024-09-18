8ea9ea16524c5bdca6b39a210d6ddd8ba4f67f00
djlamey <76072289+djlamey@users.noreply.github.com>
2024-09-18 15:48:55 -0400

    Update and rename SSLO-C01L1-ProvNetTEMP.sh to SSLO-C01L1-ProvNet.sh

--- SSLO-C01L1-ProvNet.sh
+++ SSLO-C01L1-ProvNet.sh
@@ -1,11 +1,11 @@
 # enable debugging
-set -x
+#set -x
 PS4='+$(date +"%T.%3N"): '
-
-# update Student Workstation host for the specific lab
-touch /tmp/lab1.txt
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# update Student Workstation host for the specific lab
+touch /tmp/lab1.txt
 
 # NOTHING specific for this lab - only the common lab files from common-tasks.sh
