8305961989fbb53212d63066f5bceffc72f788cb
djlamey <76072289+djlamey@users.noreply.github.com>
2024-10-18 16:35:19 -0400

    Update SSLO-QL001-Upgrade.sh

--- SSLO-QL001-Upgrade.sh
+++ SSLO-QL001-Upgrade.sh
@@ -3,9 +3,12 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 #Download needed files
-rpm=f5-iappslx-ssl-orchestrator-16.0.0-8.2.23.noarch.rpm
-mkdir --parents /home/student/Documents/SSLO/rpm
-curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/$rpm --output /home/student/Documents/SSLO/rpm/$rpm
+#rpm=f5-iappslx-ssl-orchestrator-16.0.0-8.2.23.noarch.rpm
+pkg=sslo_16.1.4-9.4.15.tgz
+#mkdir --parents /home/student/Documents/SSLO/rpm
+curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/$pkg --output /home/student/Desktop/Lab_Files/$pkg
+cd /home/student/Desktop/Lab_Files/
+tar xzf $pkg
 
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
