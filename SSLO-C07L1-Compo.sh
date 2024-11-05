d6560857d79a64c06ffa44d61404f625b443857c
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-05 10:34:14 -0500

    Update SSLO-C07L1-Compo.sh
    removed default route change, added debug off line

--- SSLO-C07L1-Compo.sh
+++ SSLO-C07L1-Compo.sh
@@ -16,7 +16,9 @@ sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
 
 # update Student Workstation
 touch /tmp/lab7.1
-sudo ip route change default via 172.16.1.33
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# disable debugging
+set +x
