4ce089e90c03076c41e4db9dbb7e515b3ebbe5c5
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-05 11:00:49 -0500

    Update SSLO-C08Lab-SecPol.sh
    set lab to be loaded with sslo1_in_app.ucs, removed default route change, added debug off line, changed lab8.1 to lab8

--- SSLO-C08Lab-SecPol.sh
+++ SSLO-C08Lab-SecPol.sh
@@ -3,7 +3,7 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 #Download needed files
-ucs=sslo1_tls13.ucs
+ucs=sslo1_in_app.ucs
 scf=swg_profile.scf
 curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/certs/RootCertAndKey.pfx --output /home/student/Desktop/Lab_Files/RootCertAndKey.pfx
 curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs --output /tmp/$ucs
@@ -28,8 +28,10 @@ sudo ssh 192.168.1.31 tmsh save sys config
 #sudo ssh 192.168.1.31 reboot
 
 # update Student Workstation
-touch /tmp/lab8.3
-sudo ip route change default via 172.16.1.33
+touch /tmp/lab8
 
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# disable debugging
+set +x
