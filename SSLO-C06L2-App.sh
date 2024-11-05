0e8ed7d12d6f6c31d2d4962d846d04c1adc893e6
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-05 17:12:00 -0500

    Update SSLO-C06L2-App.sh

--- SSLO-C06L2-App.sh
+++ SSLO-C06L2-App.sh
@@ -10,9 +10,14 @@ curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs     -
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
-#prepare sslo1
+#load sslo1 with UCS file
 sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
 sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
+
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# disable url-db auto-update
 sudo ssh 192.168.1.31 tmsh modify /sys url-db download-schedule urldb { status false }
 sudo ssh 192.168.1.31 tmsh save /sys config
 
