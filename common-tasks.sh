f83af68941cb1005ee656ace613533caef7b2d68
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-06 16:44:18 -0500

    Update common-tasks.sh

--- common-tasks.sh
+++ common-tasks.sh
@@ -27,7 +27,10 @@ bash -x /tmp/$LAB_ID.sh
 #bash /tmp/$LAB_ID.sh
 
 # common changes to jump VM
-
+im-config -n xim
+ip route show
+sudo sed --in-place 's/172.16.17.33/172.16.1.33/' /etc/netplan/01-config.yaml
+sudo netplan apply
 
 # disable debugging
 set +x
