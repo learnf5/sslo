dba84849ba520ab50ed0b7ecf930882febf8bb2d
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-06 11:54:12 -0500

    Update SSLO-C10Lab-Ha.sh
    added "active" checks and command to disable url-db auto-update on both sslo1 and sslo2

--- SSLO-C10Lab-Ha.sh
+++ SSLO-C10Lab-Ha.sh
@@ -9,7 +9,7 @@ curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/certs/RootCert
 curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs1     --output /tmp/$ucs1
 curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/ucs/$ucs2     --output /tmp/$ucs2
 
-# confirm sslo1 is active
+# confirm sslo1 and sslo2 are active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
@@ -17,14 +17,28 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Ac
 sudo scp /tmp/$ucs1 192.168.1.31:/var/local/ucs
 sudo ssh 192.168.1.31 tmsh load sys ucs $ucs1 no-license
 
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# disable url-db auto-update on sslo1
+sudo ssh 192.168.1.31 tmsh modify /sys url-db download-schedule urldb { status false }
+sudo ssh 192.168.1.31 tmsh save /sys config
+
 #prepare sslo2
 sudo scp /tmp/$ucs2 192.168.2.31:/var/local/ucs
 sudo ssh 192.168.2.31 tmsh load sys ucs $ucs2 no-license
 
+# confirm sslo2 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# disable url-db auto-update
+sudo ssh 192.168.2.31 tmsh modify /sys url-db download-schedule urldb { status false }
+sudo ssh 192.168.2.31 tmsh save /sys config
+
 # update Student Workstation
 touch /tmp/lab10
 
-# confirm bigip1 is active
+# confirm sslo1 and sslo2 are active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
