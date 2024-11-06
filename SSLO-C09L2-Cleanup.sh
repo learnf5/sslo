d5ba3e789e7a07050c3f911e6b915760efe199e5
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-06 10:48:58 -0500

    Update SSLO-C09L2-Cleanup.sh
    added "active" check and command to disable url-db auto-update

--- SSLO-C09L2-Cleanup.sh
+++ SSLO-C09L2-Cleanup.sh
@@ -14,6 +14,13 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
 sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
 
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# disable url-db auto-update
+sudo ssh 192.168.1.31 tmsh modify /sys url-db download-schedule urldb { status false }
+sudo ssh 192.168.1.31 tmsh save /sys config
+
 # update Student Workstation
 touch /tmp/lab9.2
 
