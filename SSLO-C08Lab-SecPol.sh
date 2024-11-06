45ee665b95e1ae63628c6f8a7304906f6b9966b4
djlamey <76072289+djlamey@users.noreply.github.com>
2024-11-06 10:27:55 -0500

    Update SSLO-C08Lab-SecPol.sh
    updated to match SSLO-C08L2-FwdPrxAuth.sh changes

--- SSLO-C08Lab-SecPol.sh
+++ SSLO-C08Lab-SecPol.sh
@@ -12,17 +12,23 @@ curl --silent https://raw.githubusercontent.com/learnf5/sslo/main/scf/$scf --out
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
-#prepare sslo1
+#load sslo1 with USC file
 sudo scp /tmp/$ucs 192.168.1.31:/var/local/ucs
-sudo scp /tmp/$scf 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys ucs $ucs no-license
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# provision APM on sslo1
 sudo ssh 192.168.1.31 tmsh modify sys provision apm level minimum
 
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
 # merge in swg_profile configs
+sudo scp /tmp/$scf 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file $scf
+sudo ssh 192.168.1.31 tmsh modify /sys url-db download-schedule urldb { status false }
 sudo ssh 192.168.1.31 tmsh modify /apm profile access SWG_Profile generation-action increment
 sudo ssh 192.168.1.31 tmsh save sys config
 #sudo ssh 192.168.1.31 reboot
